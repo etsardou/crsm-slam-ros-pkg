35162243d1d3ddeee27339b73b542818821110a3
Manos Tsardoulias <etsardou@gmail.com>
2014-12-16 10:26:05 +0200

    refactoring completed

--- crsm_slam/include/crsm_slam/crsm_map.h
+++ crsm_slam/include/crsm_slam/crsm_map.h
@@ -1,69 +1,79 @@
-/*
- * 	This file is part of CrsmSlam.
-    CrsmSlam is free software: you can redistribute it and/or modify 
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+/**
+  This file is part of CrsmSlam.
+  CrsmSlam is free software: you can redistribute it and/or modify 
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
 
-    CrsmSlam is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+  CrsmSlam is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with CrsmSlam.  If not, see <http://www.gnu.org/licenses/>.
-* 
-* Author : Manos Tsardoulias, etsardou@gmail.com
-* Organization : AUTH, PANDORA Robotics Team
-* */
+  You should have received a copy of the GNU General Public License
+  along with CrsmSlam.  If not, see <http://www.gnu.org/licenses/>.
+
+Author : Manos Tsardoulias, etsardou@gmail.com
+Organization : AUTH, PANDORA Robotics Team
+ **/
 
 #ifndef CRSM_MAP_HEADER
 #define CRSM_MAP_HEADER
 
-#include "crsm_mapInfo.h"
 #include <map>
 
-namespace crsm_slam{
-	
-	enum CrsmDirection{
-		RIGHT,
-		LEFT,
-		UP,
-		DOWN
-	};
+#include "crsm_mapInfo.h"
 
-	struct CrsmExpansion{
-		std::map<CrsmDirection,int> expansions;
-	};
-	
-	/**
-	 @struct CrsmMap
-	 @brief Holds the occupancy grid map
-	 **/ 
-	class CrsmMap{
-	public:
+namespace crsm_slam
+{
 
-		unsigned char **p;			//!< The container of the CRSM SLAM map. Each cell holds an occupancy probability. In the specific implementation 0 states an occupied pixel and 255 a free one.
-		CrsmMapInfo info;
-		
-		/**
-		@brief Default constructor
-		**/
-		CrsmMap(){};
-		
-		/**
-		@brief Constructor. Initialises the probability container with a specific map  size
-		@param size_ [unsigned int] The map's size in pixels
-		**/
-		CrsmMap(unsigned int size_);
-		
-		/**
-		@brief Reallocs the map to a specific direction
-		@params exp [crsm_slam::CrsmExpansion] The expansion to be made
-		@return void
-		**/
-		void expandMap(CrsmExpansion exp);
-	};
+  enum CrsmDirection
+  {
+    RIGHT,
+    LEFT,
+    UP,
+    DOWN
+  };
 
+  struct CrsmExpansion
+  {
+    std::map<CrsmDirection, int> expansions;
+  };
+
+  /**
+    @struct CrsmMap
+    @brief Holds the occupancy grid map
+   **/ 
+  class CrsmMap
+  {
+    public:
+
+      //!< The container of the CRSM SLAM map. Each cell holds an occupancy 
+      //probability. In the specific implementation 0 states an occupied pixel 
+      //and 255 a free one.
+      unsigned char **p;			
+
+      CrsmMapInfo info;
+
+      /**
+        @brief Default constructor
+       **/
+      CrsmMap(void){};
+
+      /**
+        @brief Constructor. Initialises the probability container with a 
+        specific map  size
+        @param size_ [unsigned int] The map's size in pixels
+       **/
+      CrsmMap(unsigned int size_);
+
+      /**
+        @brief Reallocs the map to a specific direction
+        @params exp [crsm_slam::CrsmExpansion] The expansion to be made
+        @return void
+       **/
+      void expandMap(CrsmExpansion exp);
+  };
 }
+
 #endif
--- crsm_slam/src/crsm_slam/crsm_map.cpp
+++ crsm_slam/src/crsm_slam/crsm_map.cpp
@@ -1,84 +1,108 @@
-/*
- * 	This file is part of CrsmSlam.
-    CrsmSlam is free software: you can redistribute it and/or modify 
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+/**
+   This file is part of CrsmSlam.
+   CrsmSlam is free software: you can redistribute it and/or modify 
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
 
-    CrsmSlam is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+   CrsmSlam is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with CrsmSlam.  If not, see <http://www.gnu.org/licenses/>.
-* 
-* Author : Manos Tsardoulias, etsardou@gmail.com
-* Organization : AUTH, PANDORA Robotics Team
-* */
+   You should have received a copy of the GNU General Public License
+   along with CrsmSlam.  If not, see <http://www.gnu.org/licenses/>.
+
+Author : Manos Tsardoulias, etsardou@gmail.com
+Organization : AUTH, PANDORA Robotics Team
+ **/
 
 #include <crsm_slam/crsm_map.h>
 
-namespace crsm_slam{
+namespace crsm_slam
+{
 
-/**
-@brief Constructor. Initialises the probability container with a specific map  size
-@param size_ [unsigned int] The map's size in pixels
-**/
-CrsmMap::CrsmMap(unsigned int size_){
-	
-	info.width=size_;
-	info.height=size_;
-	
-	info.originx=size_/2;
-	info.originy=size_/2;
-	
-	p=new unsigned char *[info.width];
-	for(unsigned int i=0;i<info.width;i++)
-		p[i]=new unsigned char [info.height];
+  /**
+    @brief Constructor. Initialises the probability container with a specific 
+    map  size
+    @param size_ [unsigned int] The map's size in pixels
+   **/
+  CrsmMap::CrsmMap(unsigned int size_)
+  {
 
-	for(unsigned int i=0;i<info.width;i++)
-		for(unsigned int j=0;j<info.height;j++)
-			p[i][j]=127;
-}
+    info.width = size_;
+    info.height = size_;
 
-/**
-@brief Reallocs the map to a specific direction
-@params exp [crsm_slam::CrsmExpansion] The expansion to be made
-@return void
-**/
-void CrsmMap::expandMap(CrsmExpansion expansion){
-	
-	
-	
-	int newOriginx=info.originx+expansion.expansions[LEFT];
-	int newOriginy=info.originy+expansion.expansions[UP];
-	
-	int newWidth=info.width+expansion.expansions[LEFT]+expansion.expansions[RIGHT];
-	int newHeight=info.height+expansion.expansions[UP]+expansion.expansions[DOWN];
-	
-	unsigned char ** newMap;
-	newMap=new unsigned char *[newWidth];
-	for(unsigned int i=0;i<newWidth;i++)
-		newMap[i]=new unsigned char[newHeight];
-	for(unsigned int i=0;i<newWidth;i++)
-		for(unsigned int j=0;j<newHeight;j++)
-			newMap[i][j]=127;
-	
-	for(unsigned int i=0;i<info.width;i++)
-		for(unsigned int j=0;j<info.height;j++)
-			newMap[i+expansion.expansions[LEFT]][j+expansion.expansions[UP]]=p[i][j];
-			
-	for(unsigned int i=0;i<info.width;i++)
-		delete [] p[i];
-	delete [] p;
+    info.originx = size_ / 2;
+    info.originy = size_ / 2;
 
-	p=newMap;
-	info.width=newWidth;
-	info.height=newHeight;
-	
-	info.originx=newOriginx;
-	info.originy=newOriginy;
-}
+    p = new unsigned char *[info.width];
+    for(unsigned int i = 0 ; i < info.width ; i++)
+    {
+      p[i] = new unsigned char [info.height];
+    }
 
+    for(unsigned int i = 0 ; i < info.width ; i++)
+    {
+      for(unsigned int j = 0 ; j < info.height ; j++)
+      {
+        p[i][j] = 127;
+      }
+    }
+  }
+
+  /**
+    @brief Reallocs the map to a specific direction
+    @params exp [crsm_slam::CrsmExpansion] The expansion to be made
+    @return void
+   **/
+  void CrsmMap::expandMap(CrsmExpansion expansion)
+  {
+
+    int newOriginx = info.originx + expansion.expansions[LEFT];
+    int newOriginy = info.originy + expansion.expansions[UP];
+
+    int newWidth = info.width + expansion.expansions[LEFT] + 
+      expansion.expansions[RIGHT];
+    int newHeight = info.height + expansion.expansions[UP] + 
+      expansion.expansions[DOWN];
+
+    unsigned char** newMap;
+    newMap = new unsigned char* [newWidth];
+
+    for(unsigned int i = 0 ; i < newWidth ; i++)
+    {
+      newMap[i] = new unsigned char[newHeight];
+    }
+    for(unsigned int i = 0 ; i < newWidth ; i++)
+    {
+      for(unsigned int j = 0 ; j < newHeight ; j++)
+      {
+        newMap[i][j] = 127;
+      }
+    }
+
+    for(unsigned int i = 0 ; i < info.width ; i++)
+    {
+      for(unsigned int j = 0 ; j < info.height ; j++)
+      {
+        newMap[i + expansion.expansions[LEFT]][j + expansion.expansions[UP]] =
+          p[i][j];
+      }
+    }
+
+    for(unsigned int i = 0 ; i < info.width ; i++)
+    {
+      delete [] p[i];
+    }
+    delete [] p;
+
+    p = newMap;
+
+    info.width = newWidth;
+    info.height = newHeight;
+
+    info.originx = newOriginx;
+    info.originy = newOriginy;
+  }
 }
